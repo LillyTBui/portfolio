ed357bf3ac42b9980e50b5dc1330f3a2cf0fd1fc
LillyTBui <73482787+LillyTBui@users.noreply.github.com>
2022-05-15 18:46:51 +0200

    sass

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ projectSection.classList.remove("slide-animation");
 
 observer.observe(section);
 observer.observe(projectSection);
+
+const navMenu = document.querySelector("#navMenu");
+const navItems = document.querySelector("#navbarSupportedContent");
+
+navMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  if (navItems.classList.contains("show")) {
+    navItems.classList.toggle("show");
+  } else {
+    setTimeout(() => {
+      navItems.classList.toggle("show");
+    }, "1500");
+  }
+});
