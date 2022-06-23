a82987179cc4132a0b1d3a6d5095576d1ac59114
LillyTBui <73482787+LillyTBui@users.noreply.github.com>
2022-06-23 13:33:33 +0200

    css update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const navButton = document.querySelector(".primary-navigation__button");
 const nav = document.querySelector(".primary-navigation");
+const logo = document.querySelector(".logo");
+const mainElement = document.querySelector("#main");
 
 navButton.addEventListener("click", () => {
   const visibility = nav.getAttribute("data-visible");
   if (visibility === "false") {
     nav.setAttribute("data-visible", true);
     navButton.setAttribute("aria-expanded", true);
+    logo.classList.add("blur");
+    mainElement.classList.add("blur");
   } else {
     nav.setAttribute("data-visible", false);
     navButton.setAttribute("aria-expanded", false);
+    logo.classList.remove("blur");
+    mainElement.classList.remove("blur");
   }
 });
