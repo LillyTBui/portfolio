727bb7b2a6c0bf8ad58af8d4a30d47785ff93b97
LillyTBui <73482787+LillyTBui@users.noreply.github.com>
2022-07-04 22:41:24 +0200

    loader

--- js/index.js
+++ js/index.js
@@ -7,3 +7,27 @@ projectSection.classList.remove("slide-animation");
 
 observer.observe(section);
 observer.observe(projectSection);
+
+/* remove loader after animation is done */
+const loader = document.querySelector(".loading-logo");
+const lastLetter = document.getElementById("B");
+const header = document.querySelector("header");
+const main = document.querySelectorAll("main > :not(.loading-logo");
+
+header.classList.add("hideElement");
+main.forEach((element) => {
+  element.classList.add("hideElement");
+});
+
+lastLetter.addEventListener("animationend", function () {
+  loader.style.opacity = "0";
+  loader.style.transition = "opacity 0.8s ease-out";
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 1000);
+
+  header.classList.remove("hideElement");
+  main.forEach((element) => {
+    element.classList.remove("hideElement");
+  });
+});
